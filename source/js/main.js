42f243020f207018e6677b6117cb72d5b4e7daf6
Maxim Krasnobaev <toyscrew1@gmail.com>
2023-05-10 16:41:37 +0300

    Частично сделана разметка для декстоп версии

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {Burger} from './utils/burger';
 
 // ---------------------------------
 
@@ -10,6 +10,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  const burger = new Burger();
+  burger.init();
 
   // Modules
   // ---------------------------------
@@ -17,7 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     const form = new Form();
     window.form = form;
     form.init();
