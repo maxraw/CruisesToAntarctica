305c5d98232fd28c2ce3371480da619000e93f38
Maxim Krasnobaev <toyscrew1@gmail.com>
2023-05-11 10:22:47 +0300

    Десктоп версия за исключением карты

--- source/js/main.js
+++ source/js/main.js
@@ -22,6 +22,38 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+
+    let myMap;
+
+    // Дождёмся загрузки API и готовности DOM.
+    ymaps.ready(init);
+
+    function init() {
+      // Создание экземпляра карты и его привязка к контейнеру с
+      // заданным id ("map").
+      myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [59.93863106417265, 30.323036499999905],
+        zoom: 16,
+      }, {
+        searchControlProvider: 'yandex#search',
+      });
+
+      let myPlacemark1 = new ymaps.Placemark([59.93863106417265, 30.323036499999905], {
+        balloonContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8',
+      }, {
+        iconLayout: 'default#image',
+        iconImageClipRect: [[0, 0], [26, 47]],
+        iconImageHref: 'img/svg/icon-pin.svg',
+        iconImageSize: [18, 22],
+        iconImageOffset: [-15, -27],
+        iconShadow: false,
+      });
+
+      myMap.geoObjects.add(myPlacemark1);
+
+    }
   });
 });
 
